a383e62e4b83737dc41c19f394bd653ec58e900c
Don Corley <don@donandann.com>
2020-01-21 22:03:24 -0800

    test

---
+++ src/main/java/org/jibx/schema/test/person/Test.java
@@ -0,0 +1,13 @@
+package org.jibx.schema.test.person;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test
+{
+
+    public static final void main(String[] args)
+    {
+    	System.out.println("Hello World!");
+    }
+}
